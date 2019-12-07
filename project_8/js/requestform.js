8382d2b93b0179bb625dbeaead0761d88c38fe8b
lillianadesouza <lillianadesouza2021@u.northwestern.edu>
2019-12-07 13:17:13 -0600

    final changes to p6 component, adds submission page

--- project_8/js/requestform.js
+++ project_8/js/requestform.js
@@ -1,3 +1,7 @@
 function submitPopup() {
     alert("Thank you for submitting your communication form! \nClick 'OK' to submit another form, or press back to return to homepage.");
 }
+
+function submitChange(){
+    document.location.href = "./requestform-completion.html";
+}
