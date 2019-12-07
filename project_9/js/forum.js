fff42240c280570c014c32044f88fd5c062c8397
Yibo Pan <yibopan12@gmail.com>
2019-12-07 15:32:56 -0600

    final project_9

--- project_9/js/forum.js
+++ project_9/js/forum.js
@@ -4,3 +4,15 @@ const submitPopup = () => {
     window.localStorage.setItem('points', points)
     alert("Thanks for submitting your message! Now you have " + points + ' points in total!')
 }
+
+function navProfile(){
+    username = window.localStorage.getItem('username')
+    if (username == "user1"){
+        document.location.href = "./profile_1.html";
+    }
+    else{
+        document.location.href = "./profile_2.html"
+    }
+
+  }
+
